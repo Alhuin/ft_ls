f0fe71ad9bccd044e3c2a75d81e1d7e7a3d0d208
Julien Janin-reynaud <jjanin-r@z2r2p5.le-101.fr>
2018-03-06 11:54:34 +0100

    version secure

---
+++ ft_ls.h
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_ls.h                                          .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: jjanin-r <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/02/14 13:47:30 by jjanin-r     #+#   ##    ##    #+#       */
+/*   Updated: 2018/02/27 21:58:03 by jjanin-r    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+# define FT_LS_H
+# include "Libft/ft_libft/libft.h"
+# include <sys/stat.h>
+# include <grp.h>
+# include <pwd.h>
+# include <time.h>
+# include <dirent.h>
+# include <errno.h>
+# include <stdio.h>
+
+typedef struct		s_tree
+{
+	struct s_file	*file;
+	struct s_tree	*left;
+	struct s_tree	*right;
+}					t_tree;
+
+typedef struct		s_file
+{
+	struct stat		sb;
+	int				arg;
+	int				exec;
+	int				total;
+	char			*name;
+	char			*path;
+	t_tree			*subtree;
+	t_tree			*alphatime;
+}					t_file;
+
+typedef struct		s_flags
+{
+	int				arg;
+	int				l;
+	int				a;
+	int				r;
+	int				bigr;
+	int				t;
+}					t_flags;
+
+char				*ft_buildpath(char *arg, char *name);
+char				*ft_get_perms(mode_t mode);
+int					ft_getdirstats(t_file **file, char *arg, t_flags *flags);
+int					ft_createnode(t_file **file, t_tree **tocreate);
+int					ft_addnode(t_file **file, t_tree **node);
+int					ft_raddnode(t_file **file, t_tree **node);
+int					ft_taddnode(t_file **file, t_tree **node);
+int					ft_traddnode(t_file **file, t_tree **node);
+int					ft_fill_tree(t_file **file, t_tree **tree, t_flags *flags);
+void				ft_print_name(t_tree *tree, int lengh, t_flags *flags);
+void				ft_print_tree(t_tree *tree, int lengh, t_flags *flags);
+void				ft_computeargs(t_tree *tree, t_flags **flags);
+int					ft_printl(t_file **file, t_flags **flags);
+#endif
---
+++ print.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   print.c                                          .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: jjanin-r <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/02/27 19:00:54 by jjanin-r     #+#   ##    ##    #+#       */
+/*   Updated: 2018/02/28 16:52:26 by jjanin-r    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "ft_ls.h"
+
+void		ft_print_name(t_tree *tree, int lengh, t_flags *flags)
+{
+	if (flags->l != 1)
+	{
+		if (tree->file->name[0] != '.' || flags->a == 1)
+		{
+			if (S_ISLNK(tree->file->sb.st_mode))
+				ft_printf("{magenta}%-*s{eoc}", lengh, tree->file->name);
+			else if (S_ISDIR(tree->file->sb.st_mode))
+				ft_printf("{bcyan}%-*s{eoc}", lengh, tree->file->name);
+			else if (tree->file->sb.st_mode & S_IXUSR)
+				ft_printf("{red}%-*s{eoc}", lengh, tree->file->name);
+			else
+				ft_printf("%-*s", lengh, tree->file->name);
+		}
+	}
+	else
+		ft_printl(&(tree->file),  &flags);
+}
+
+void		ft_print_tree(t_tree *tree, int lengh, t_flags *flags)
+{
+	if (tree != NULL)
+	{
+		if (tree->left)
+			ft_print_tree(tree->left, lengh, flags);
+		if (tree->file->alphatime == NULL)
+			ft_print_name(tree, lengh, flags);
+		else
+			ft_print_tree(tree->file->alphatime, lengh, flags);
+		if (tree->right)
+			ft_print_tree(tree->right, lengh, flags);
+	}
+}
+
+void		ft_print_date(char *time, char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == 'M')
+			ft_printf("%4.3s", time + 4);
+		if (str[i] == 'D')
+			ft_printf("%3.2s", time + 8);
+		if (str[i] == 'H')
+			ft_printf("%6.5s", time + 11);
+		i++;
+	}
+}
+
+int			ft_printl(t_file **file, t_flags **flags)
+{
+	struct group	*grp;
+	struct passwd	*pwd;
+	char			*time;
+
+	if ((*file)->name[0] != '.' || (*flags)->a == 1)
+	{
+		ft_printf("%s", ft_get_perms((*file)->sb.st_mode));
+		ft_printf("%4ld", (*file)->sb.st_nlink);
+		grp = getgrgid((*file)->sb.st_gid);
+		pwd = getpwuid((*file)->sb.st_uid);
+		ft_printf("%9s", pwd->pw_name);
+		ft_printf("%9s", grp->gr_name);
+		ft_printf("%8lld", (*file)->sb.st_size);
+		time = ctime(&(*file)->sb.st_mtime);
+		ft_print_date(time, "MDH");
+		if (S_ISDIR((*file)->sb.st_mode))
+			ft_printf(" {bcyan}%s{eoc}\n", (*file)->name);
+		else if ((*file)->sb.st_mode & S_IXUSR)
+			ft_printf(" {red}%s{eoc}\n", (*file)->name);
+		else
+			ft_printf(" %s\n", (*file)->name);
+	}
+	return (0);
+}
